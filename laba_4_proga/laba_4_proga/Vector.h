0f0164c9625f6933a5757a066ebd5cd7baccdb36
A-no-maliya <126778037+A-no-maliya@users.noreply.github.com>
2024-03-19 21:57:09 +0300

    all workingVector.h

--- laba_4_proga/laba_4_proga/Vector.h
+++ laba_4_proga/laba_4_proga/Vector.h
@@ -1,35 +1,34 @@
-// Vector.h
-#pragma once // Защита от повторного включения файла
-#include "new_matrices.h" // Подключение определения класса Matrix
+#pragma once // Предотвращает многократное включение файла заголовка
+#include "new_matrices.h" // Подключает определение базового класса Matrix
 
-// Шаблонный класс Vector наследуется от шаблонного класса Matrix
-template<typename T>
-class Vector : public Matrix<T> {
+template<typename T> // Определяет шаблон класса Vector с типом данных T
+class Vector : public Matrix<T> { // Наследует от шаблона класса Matrix
 public:
-    
-    Vector(unsigned int n) : Matrix<T>(1, n) {} // Конструктор вектора, инициализирующий матрицу размером 1xN для представления вектора
+    // Конструктор класса Vector. Инициализирует базовый класс Matrix однострочной матрицей.
+    Vector(unsigned int n) : Matrix<T>(1, n) {}
 
-    // Метод для вычисления скалярного произведения двух векторов
+    // Метод для вычисления скалярного произведения двух векторов.
     T dotProduct(const Vector& other) const {
-        T sum = 0;                                          // Инициализация суммы для результата
-        for (unsigned int i = 0; i < this->n; ++i) {        // Перебор всех элементов вектора
-            sum += this->data[0][i] * other.data[0][i];     // Умножение соответствующих элементов и добавление результата в сумму
+        T sum = 0;                                                      // Начальное значение суммы для скалярного произведения 
+        for (unsigned int i = 0; i < this->getCols(); ++i) {            // Проходим по всем элементам вектора
+            sum += this->at(0, i) * other.at(0, i);                     // Добавляем произведение соответствующих элементов к сумме
         }
-        return sum; // Возвращение результата скалярного произведения
+        return sum;                             // Возвращаем сумму как результат скалярного произведения
     }
 
-    // Метод для вычисления векторного произведения двух векторов (определено только для 3D векторов)
+    // Метод для вычисления векторного произведения двух векторов
     Vector crossProduct(const Vector& other) const {
-        if (this->n != 3 || other.n != 3) {                                                 // Проверка, что оба вектора трехмерные
-            throw std::invalid_argument("Cross product is defined for 3D vectors only.");   // Исключение, если условие не выполнено
+
+        // Проверяем, что размерность обоих векторов равна 3
+        if (this->getCols() != 3 || other.getCols() != 3) {
+            throw std::invalid_argument("Cross product is defined for 3D vectors only."); // Генерируем исключение, если векторы не трехмерные
         }
-        Vector<T> result(3);                                                                // Создание результата векторного произведения
-        
-        // Вычисление компонентов векторного произведения согласно формулам
-        result.data[0][0] = this->data[0][1] * other.data[0][2] - this->data[0][2] * other.data[0][1];
-        result.data[0][1] = this->data[0][2] * other.data[0][1] - this->data[0][1] * other.data[0][2];
-        result.data[0][2] = this->data[0][0] * other.data[0][1] - this->data[0][1] * other.data[0][0];
-        return result; // Возвращение результата векторного произведения
+
+        Vector<T> result(3); // Создаем вектор для результата векторного произведения
+        // Вычисляем компоненты результата векторного произведения
+        result.at(0, 0) = this->at(0, 1) * other.at(0, 2) - this->at(0, 2) * other.at(0, 1);
+        result.at(0, 1) = this->at(0, 2) * other.at(0, 1) - this->at(0, 1) * other.at(0, 2);
+        result.at(0, 2) = this->at(0, 0) * other.at(0, 1) - this->at(0, 1) * other.at(0, 0);
+        return result; // Возвращаем вектор-результат.
     }
 };
-
